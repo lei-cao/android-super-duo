4c727734f887f1a82825b5fa4c6db0c069b1b927
Lei Cao <lei.cao@zalora.com>
2016-01-10 17:53:19 +0800

    Added widget

---
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/widget/TodayWidgetIntentService.java
@@ -0,0 +1,122 @@
+package barqsoft.footballscores.widget;
+
+import android.annotation.TargetApi;
+import android.app.IntentService;
+import android.app.PendingIntent;
+import android.appwidget.AppWidgetManager;
+import android.content.ComponentName;
+import android.content.Intent;
+import android.database.Cursor;
+import android.os.Build;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.widget.RemoteViews;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import barqsoft.footballscores.DatabaseContract;
+import barqsoft.footballscores.MainActivity;
+import barqsoft.footballscores.R;
+import barqsoft.footballscores.Utilies;
+
+public class TodayWidgetIntentService extends IntentService {
+
+    public TodayWidgetIntentService() {
+        super("TodayWidgetIntentService");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        // Retrieve all of the Today widget ids: these are the widgets we need to update
+        AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
+        int[] appWidgetIds = appWidgetManager.getAppWidgetIds(new ComponentName(this, TodayWidgetProvider.class));
+
+        // Get today's data from the ContentProvider
+        Date fragmentdate = new Date(System.currentTimeMillis());
+        SimpleDateFormat mformat = new SimpleDateFormat("yyyy-MM-dd");
+        String mDate = mformat.format(fragmentdate);
+
+        String mSelectionClause = DatabaseContract.scores_table.DATE_COL + ">= ?";
+        String[] selectionArgs = {mDate};
+
+        Cursor data = getContentResolver().query(DatabaseContract.BASE_CONTENT_URI, null, mSelectionClause, selectionArgs, DatabaseContract.scores_table.TIME_COL + " DESC");
+
+        if (data == null) {
+            return;
+        }
+        if (!data.moveToFirst()) {
+            data.close();
+            return;
+        }
+
+
+        // Extract the match data from the Cursor
+        String description = "Descriptions~";
+        int id = data.getInt(0);
+        String date = data.getString(1);
+        String time = data.getString(2);
+        String home = data.getString(3);
+        String away = data.getString(4);
+        String league = data.getString(5);
+        int homeGoals = data.getInt(6);
+        int awayGoals = data.getInt(7);
+        String matchId = data.getString(8);
+        String matchDay = data.getString(9);
+        // Perform this loop procedure for each Today widget
+        for (int appWidgetId : appWidgetIds) {
+            // Find the correct layout based on the widget's width
+            int widgetWidth = getWidgetWidth(appWidgetManager, appWidgetId);
+            int defaultWidth = getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+            int largeWidth = getResources().getDimensionPixelSize(R.dimen.widget_today_large_width);
+            int layoutId = R.layout.widget_today;
+            RemoteViews views = new RemoteViews(getPackageName(), layoutId);
+
+            // Add the data to the RemoteViews
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1) {
+                setRemoteContentDescription(views, description);
+            }
+            views.setTextViewText(R.id.home_name, home);
+            views.setTextViewText(R.id.away_name, away);
+            views.setTextViewText(R.id.data_textview, time);
+            views.setTextViewText(R.id.score_textview, Utilies.getScores(homeGoals, awayGoals));
+            views.setImageViewResource(R.id.home_crest, Utilies.getTeamCrestByTeamName(home));
+            views.setImageViewResource(R.id.away_crest, Utilies.getTeamCrestByTeamName(away));
+
+            // Create an Intent to launch MainActivity
+            Intent launchIntent = new Intent(this, MainActivity.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, launchIntent, 0);
+            views.setOnClickPendingIntent(R.id.widget, pendingIntent);
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
+        }
+    }
+
+    private int getWidgetWidth(AppWidgetManager appWidgetManager, int appWidgetId) {
+        // Prior to Jelly Bean, widgets were always their default size
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
+            return getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+        }
+        // For Jelly Bean and higher devices, widgets can be resized
+        // the current size can be retrieved from the newly added App Widget Options
+        return getWidgetWidthFromOptions(appWidgetManager, appWidgetId);
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
+    private int getWidgetWidthFromOptions(AppWidgetManager appWidgetManager, int appWidgetId) {
+        Bundle options = appWidgetManager.getAppWidgetOptions(appWidgetId);
+        if (options.containsKey(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH)) {
+            int minWidthDp = options.getInt(AppWidgetManager.OPTION_APPWIDGET_MIN_WIDTH);
+            // The width returned is in dp, but we'll convert it to pixels to match the other widths
+            DisplayMetrics displayMetrics = getResources().getDisplayMetrics();
+            return (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, minWidthDp, displayMetrics);
+        }
+        return getResources().getDimensionPixelSize(R.dimen.widget_today_default_width);
+    }
+
+    @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH_MR1)
+    private void setRemoteContentDescription(RemoteViews views, String description) {
+    }
+}
